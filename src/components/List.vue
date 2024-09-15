0e739bb2388a34909d7fc619ed7bfc647121962a
jasonbai <jasonbai@noreply.gitcode.com>
2024-09-15 21:16:01 +0800

    联调接口

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="list-wrap">
-    <div class="item" v-for="(n,i) in listData" :key="i">
+    <div class="item" v-for="(n, i) in listData" :key="i">
       <span>{{ n.index }}.</span>
-      <a :href="n.mobilUrl" class="title" target="_blank">{{ n.title }}</a>
+      <a :href="n.url" class="title" target="_blank">{{ n.title }}</a>
     </div>
     <p>{{ updateDate }}</p>
   </div>
@@ -30,15 +30,18 @@ export default {
 }
 .item {
   text-align: left;
-  margin-bottom: 17px;
+  margin-bottom: 20px;
   display: flex;
   align-items: baseline;
   span {
     display: inline-block;
-    margin-right: 5px;
-    width: 23px;
+    margin-right: 7px;
+    width: 26px;
     text-align: right;
     flex-shrink: 0;
+    font-size: 22px;
+    font-style: italic;
+    color: #555;
   }
   a {
     color: #409eff;
@@ -46,6 +49,7 @@ export default {
     text-align: justify;
     line-height: 25px;
     text-decoration: none;
+    letter-spacing: 1px;
   }
 }
 p {
